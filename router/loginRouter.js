14b92eb444c6b0344d7cd24869dcbc8600d60e66
nahid9764 <nahidhossain9764@gmail.com>
2022-08-27 22:35:05 +0600

    verifyUserByCookie added

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,9 +2,10 @@
 const express = require("express");
 
 //internal import
-const { getLogin, login, logout } = require("../controller/loginController");
+const { getLogin, login, logout, verifyUserByCookie } = require("../controller/loginController");
 const decorateHtmlRes = require("../middleware/common/decorateHtmlRes");
 const { loginValidators, loginValidationHandler } = require("../middleware/login/loginValidators");
+const { checkLogin } = require("../middleware/common/checkLogin");
 
 const router = express.Router();
 
@@ -13,6 +14,9 @@ router.get("/", decorateHtmlRes("Login"), getLogin);
 
 // process login
 router.post("/login", loginValidators, loginValidationHandler, login);
+
+// re-validate user by only token
+router.get("/validate-auth", checkLogin, verifyUserByCookie);
 
 // logout
 router.delete("/logout", logout);
